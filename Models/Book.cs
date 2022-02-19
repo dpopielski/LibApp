dcea4f2e68a8e77be6a76a7d01d6ce0d00a5d394
John Doe <damianpopielski.dp@gmail.com>
2022-02-19 11:22:13 +0100

    Add validation of data to BookForm

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,17 +9,22 @@ namespace LibApp.Models
 {
     public class Book
     {
-        public int Id { get; set; }
-		[Required]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Please enter name")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Please enter author name")]
+		[StringLength(255)]
 		public string AuthorName { get; set; }
-		[Required]
 		public Genre Genre { get; set; }
+		[Required(ErrorMessage = "Please select Genre")]
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
+		[Required(ErrorMessage = "Please enter data")]
 		public DateTime ReleaseDate { get; set; }
+		[Range(1,20)]
+		[Required(ErrorMessage = "Please enter number")]
 		public int NumberInStock { get; set; }
 		public int NumberAvailable { get; set; }
 	}
