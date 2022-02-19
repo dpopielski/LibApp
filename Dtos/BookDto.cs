dba593204e19df8ea54d0721b4b941a831fd3b16
John Doe <damianpopielski.dp@gmail.com>
2022-02-19 14:32:51 +0100

    Add authorization and authentication to application

--- Dtos/BookDto.cs
+++ Dtos/BookDto.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using LibApp.Models;
 
 namespace LibApp.Dtos
 {
     public class BookDto
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        public byte GenreId { get; set; }
+        public GenreTypeDto Genre { get; set; }
     }
 }
